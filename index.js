4debbc985e70c3a8a3fcfff4bacc9efaef603a51
Suraj Pawar <suraj.leadsoft@gmail.com>
2025-04-10 09:12:45 +0530

    main

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./models/User')
+
+const server = express()
+server.use(cors())
+server.use(bodyParser.json())
+
+// Connect Atlast Cloud DB 
+mongoose.connect('mongodb+srv://suraj:Suraj%40123@cluster0.flnskgw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').
+then(()=>console.log('Database connected ')).
+catch((err)=>console.log(err))
+
+server.post('/register',async(req,res)=>{
+    try{
+        const {fullName,userName,age,password} = req.body
+        const userExist = await User.findOne({userName})
+        if(userExist){
+            return res.json({
+                status:false,
+                message:'User already exists !!'
+            })
+        }
+        const userObj = new User({fullName,userName,age,password})
+        await userObj.save()
+        res.json({
+            status:true,message:'User registered successfully !!'
+        })
+    }
+    catch(err){
+        res.json({
+            status:false,
+            message:`Error:${err}`
+        })
+    }
+})
+
+server.post('/login',async(req,res)=>{
+    try{
+        const {userName,password} = req.body
+        const userExist = await User.findOne({userName})
+        if(!userExist){
+            return res.json({status:false,message:'User not found !!'})
+        }
+        if(password !== userExist.password){
+            return res.json({status:false,message:'Wrong password !!'})
+        }
+        res.json({status:true,message:'Login successfull !!'})
+    }
+    catch(err){
+        res.json({
+            status:false,
+            message:err
+        })
+    }
+})
+
+server.listen(8055,()=>{
+    console.log('Server started listening on port 8055')
+})
+
